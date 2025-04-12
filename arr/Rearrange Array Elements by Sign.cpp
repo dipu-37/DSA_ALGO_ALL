4f6f54fdc70758d54f1021ca90fcba825a808192
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-04-12 16:09:38 +0600

    leader of an array

--- arr/Rearrange Array Elements by Sign.cpp
+++ arr/Rearrange Array Elements by Sign.cpp
@@ -8,18 +8,20 @@ void RearrangeArrayElementsbySign(int arr[],int n)
     vector<int>pos;
     vector<int>neg;
 
-    for(int i=0;i<n; i++)
+    for(int i=0; i<n; i++)
     {
         if(arr[i]>0)
         {
             pos.push_back(arr[i]);
-        }else{
+        }
+        else
+        {
             neg.push_back(arr[i]);
         }
     }
 
     /// positive index = 2*i ; negative index = 2*i +1;   i = 0....
-    for(int i=0;i<n/2; i++)
+    for(int i=0; i<n/2; i++)
     {
         arr[2*i]= pos[i];
         arr[2*i + 1] = neg[i];
@@ -35,24 +37,83 @@ vector<int> RearrangeArrayElementsbySign1(int arr[],int n)
     /// vector<int>v(n,0) or vector<int>(n)  ---> not allow for push_back()   only allow v[i]= something;
     /// vector<int>v = {1,2,3,}   both but v[i] == access or modify  ;   push_back()  ==== push in the last
     vector<int>ans(n,0);
-    int posIndex = 0 , negIndex = 1;
+    int posIndex = 0, negIndex = 1;
 
-    for(int i=0;i<n; i++)
+    for(int i=0; i<n; i++)
     {
         if(arr[i]>0)
         {
-           ans[posIndex] = arr[i];
-           posIndex += 2;
-        }else{
-           ans[negIndex] = arr[i];
-           negIndex += 2;
+            ans[posIndex] = arr[i];
+            posIndex += 2;
+        }
+        else
+        {
+            ans[negIndex] = arr[i];
+            negIndex += 2;
         }
     }
 
-   return ans ;
+    return ans ;
 
 }
 
+
+
+/// poroblem version - 02;
+
+///  positive != negative    pos > neg or neg > pos
+
+void RearrangeArrayElementsbySign(int arr[], int n)
+{
+    vector<int>pos, neg;
+
+    for(int i=0 ; i<n; i++)
+    {
+        if(arr[i]> 0)
+        {
+            pos.push_back(arr[i]);
+        }
+        else
+        {
+            neg.push_back(arr[i]);
+        }
+    }
+
+    if(pos.size() < neg.size())
+    {
+        for(int i=0 ; i< pos.size() ; i++)
+        {
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+
+        int index = pos.size() *2;
+
+        for(int i=pos.size(); i< neg.size(); i++)
+        {
+            arr[index] = neg[i];
+            index ++;
+        }
+    }else{
+
+        for(int i=0; i<neg.size(); i++)
+        {
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i]
+        }
+
+        int index = neg.size()*2;
+
+        for(int i= neg.size(); i<pos.size(); i++)
+        {
+            arr[index] = pos[i];
+            index++;
+        }
+
+    }
+
+    ///return arr;
+}
 
 
 int main()
@@ -68,7 +129,8 @@ int main()
 
     vector<int>result = RearrangeArrayElementsbySign1(arr, n);
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++)
+    {
         cout<<result[i]<<" ";
     }
 
---
+++ arr/leader.cpp
@@ -0,0 +1,79 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+//
+//vector<int> printLeadersBruteForce(int arr[], int n)
+//{
+//    vector<int>ans;
+//
+//    for(int i=0; i<n; i++)
+//    {
+//        bool leader = true;
+//
+//        for(int j=i+1; j<n; j++)
+//        {
+//            if(arr[j]>arr[i])
+//            {
+//                leader = false;
+//                break;
+//            }
+//        }
+//        if(leader)
+//        {
+//            ans.push_back(arr[i]);
+//        }
+//
+//    }
+//
+//    return ans;
+//
+//}
+
+vector<int> printLeadersOptimal(int arr[],int n)
+{
+    vector<int>ans;
+
+    int maxi = arr[n-1];
+    ans.push_back(arr[n-1]);
+
+    for(int i=n-2; i>=0; i--)
+    {
+        if(arr[i]>maxi){
+            ans.push_back(arr[i]);
+            maxi=arr[i];
+        }
+
+        /// maxi=max(maxi,arr[i])
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >>arr[i];
+    }
+
+    //vector<int>ans = printLeadersBruteForce(arr,n);
+    vector<int>ans = printLeadersOptimal(arr,n);
+    for(int i = 0; i<ans.size(); i++)
+    {
+
+        cout<<ans[i]<<" ";
+    }
+
+
+
+}
+
+//6
+//10 22 12 3 0 6
+//22 12 6
+
+

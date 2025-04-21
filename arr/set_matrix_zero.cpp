903db182c1699f7c014dff0d5949b691e0fbdf35
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-04-21 22:14:17 +0600

    spairal matrix

--- arr/set_matrix_zero.cpp
+++ arr/set_matrix_zero.cpp
@@ -2,11 +2,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+/// brute force approach
+
 void makeRow(vector<vector<int>>&arr,int n, int m, int i)
 {
-    for(int j=0;j<m;j++)
+    for(int j=0; j<m; j++)
     {
-        if(arr[i][j]!=0){
+        if(arr[i][j]!=0)
+        {
             arr[i][j]=-1;
         }
     }
@@ -14,9 +18,10 @@ void makeRow(vector<vector<int>>&arr,int n, int m, int i)
 
 void makeCol(vector<vector<int>>&arr,int n, int m, int j)
 {
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        if(arr[i][j]!=0){
+        if(arr[i][j]!=0)
+        {
             arr[i][j]=-1;
         }
     }
@@ -25,36 +30,76 @@ void makeCol(vector<vector<int>>&arr,int n, int m, int j)
 vector<vector<int>>setMatrixZero(vector<vector<int>>&arr,int n, int m)
 {
 
-   for(int i=0;i<n;i++)
-   {
-       for(int j=0;j<m;j++)
-       {
-           if(arr[i][j]==0){
-            makeRow(arr,n,m,i);
-            makeCol(arr,n,m,j);
-           }
-       }
-   }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(arr[i][j]==0)
+            {
+                makeRow(arr,n,m,i);
+                makeCol(arr,n,m,j);
+            }
+        }
+    }
 
-   for(int i=0;i<n;i++)
-   {
-       for(int j=0;j<m;j++)
-       {
-           if(arr[i][j]==-1){
-            arr[i][j]=0;
-           }
-       }
-   }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(arr[i][j]==-1)
+            {
+                arr[i][j]=0;
+            }
+        }
+    }
 
-   return arr;
+    return arr;
 }
-int main(){
+
+
+/// better approach
+
+vector<vector<int>>setMatrixZero(vector<vector<int>>&arr,int n, int m)
+{
+    int row[n] = {0}; // row array
+    int col[m] = {0};  // col array
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(arr[i][j]==0)
+            {
+                row[i] =1;
+                col[i] =1;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(row[i] || col[j])
+            {
+                arr[i][j] =0;
+            }
+
+        }
+    }
+
+}
+
+
+
+int main()
+{
     int n, m;
     cin>>n>>m;
     vector<vector<int>>arr(n,vector<int>(m));
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        for(int j=0;j<m;j++)
+        for(int j=0; j<m; j++)
         {
             cin>>arr[i][j];
         }
@@ -62,8 +107,10 @@ int main(){
 
     vector<vector<int>>ans= setMatrixZero(arr,n,m);
 
-    for(auto it : ans){
-        for(auto ele : it){
+    for(auto it : ans)
+    {
+        for(auto ele : it)
+        {
             cout<<ele<<" ";
         }
         cout<<endl;
---
+++ arr/spiral_traversal_of_matrix.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> printSpiral( vector<vector<int>>&mat)
+{
+    vector<int>ans;
+    int n = mat.size();
+    int m = mat[0].size();
+
+    int top =0,left = 0, bottom = n-1, right = m-1;
+
+    while(top<=bottom && left <= right)
+    {
+        ///right
+        for(int i=left; i<=right;i++)
+        {
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+
+        ///bottom
+        for(int i=top; i<=bottom; i++)
+        {
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+
+        ///left
+
+        if(top<=bottom)
+        {
+            for(int i=right; i>=left; i--)
+            {
+                ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
+        }
+
+        /// top
+
+        if(left<=right){
+            for(int i=right;i>=left;i--){
+                ans.push_back(mat[bottom][i]);
+            }
+            left++;
+        }
+
+        return ans;
+
+    }
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>mat(n,vector<int>(m));
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>>mat[i][j];
+        }
+    }
+    vector<int>ans = printSpiral(mat);
+}
